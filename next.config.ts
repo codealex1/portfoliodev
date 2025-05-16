4d4c40256163c2c229ba779b94bdbd52fa4879fd
codealex1 <148983986+codealex1@users.noreply.github.com>
2025-05-16 11:26:29 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // ✅ C'est ce qui remplace "next export"
+  reactStrictMode: true,
 };
 
 export default nextConfig;
